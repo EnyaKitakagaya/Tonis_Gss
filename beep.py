b64c18920250c6892e7c1dc5ff4338749ca30706
Penkich <nishijim2001@yahoo.co.jp>
2017-02-11 13:33:53 +0900

    Update beep.py

--- beep.py
+++ beep.py
@@ -10,10 +10,10 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(buzpin,GPIO.OUT)
 
-def buz(cnt):
+def beep(cnt):
     for i in range(cnt):
         GPIO.output(buzpin,True)
         time.sleep(0.2)
         GPIO.output(buzpin,False)
         time.sleep(0.2)
-buz(2)
+beep(2)
