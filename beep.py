e7de5fa93e8b7eafc374483a8e6e5b3a5ae661b9
root <root@raspberrypi.lan>
2017-02-11 12:43:21 +0900

    test

---
+++ beep.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import time
+
+buzpin = 23
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(buzpin,GPIO.OUT)
+
+def buz(cnt):
+    for i in range(cnt):
+        GPIO.output(buzpin,True)
+        time.sleep(0.2)
+        GPIO.output(buzpin,False)
+        time.sleep(0.2)
+buz(2)
---
+++ warnbeep.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import time
+
+buzpin = 23
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(buzpin,GPIO.OUT)
+
+def warnbuz(cnt):
+    for i in range(cnt):
+        GPIO.output(buzpin,True)
+        time.sleep(0.1)
+        GPIO.output(buzpin,False)
+        time.sleep(0.1)
+
+warnbuz(5)
